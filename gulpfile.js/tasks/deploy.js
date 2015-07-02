d06ea35203ab94c001f548abf887884666fadfab
irideas <irideas@gmail.com>
2015-07-02 15:00:55 +0800

    fix deploy task

--- gulpfile.js/tasks/deploy.js
+++ gulpfile.js/tasks/deploy.js
@@ -19,7 +19,7 @@ gulp.task('deploy::clean' , function(cb){
 });	
 
 gulp.task('deploy::ready' , function(cb){
-	return gulp.src([config.destDirectory+'/apps/'+appkey+'/**/*' , config.destDirectory+'/+(framework|module)/**/*' ])
+	return gulp.src([config.destDirectory+'/apps/'+appkey+'/**/*' , config.destDirectory+'/+(framework|module|vendor)/**/*' ])
   		.pipe(plumber())
     	.pipe(gulp.dest(config.tmpDirectory));
 });
